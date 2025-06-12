8dec4659d6de32582c7ec3140bfa89568fa97879
Cennge <ilyagliba02@gmail.com>
2025-06-12 12:16:52 +0300

    decomposing, edited some func, fixes

--- src/components/Home-Page.tsx
+++ src/components/Home-Page.tsx
@@ -1,11 +1,12 @@
 import BGObjects from './BG-Objects';
 import './Home-Page.css'
+import {Journey} from './Journey'
 
 export function Home() {
     return (
         <div>
             <div className='sec'>
-                <Section6/>
+                <Journey/>
             </div>
 
             <div className='sec'>
@@ -19,25 +20,4 @@ export function Home() {
     );
 } 
 
-export const Section6 = () => {
-  return (
-    <div className="home-page">
-      <div className="hero-wrapper">
-        <section className="hero-container">
-               <img src="/bg-object-sec6.png" alt="Background" className="section6-image" />
-          <div className="hero-content">
-            <h1 className="hero-heading">
-              Start your financial journey with <span className="highlight">4epuBank</span> today!
-            </h1>
-            <p className="hero-subtext">
-              Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus pellentesque elit.
-              Pellentesque eget ut imperdiet nulla penatibus. Nascetur viverra arcu sed amet cursus purus.
-            </p>
-          </div>
-          <button className="hero-button">Open Account</button>
-    
-        </section>
-      </div>
-    </div>
-  );
-};
+
---
+++ src/components/Journey.tsx
@@ -0,0 +1,24 @@
+import './Journey.css'
+
+export const Journey = () => {
+  return (
+    <div className="home-page-journey">
+      <div className="hero-wrapper-journey">
+        <section className="hero-container-journey">
+               <img src="/bg-object-sec6.png" alt="Background" className="image-journey" />
+          <div className="hero-content-journey">
+            <h1 className="hero-heading-journey">
+              Start your financial journey with <span className="highlight-journey">4epuBank</span> today!
+            </h1>
+            <p className="hero-subtext-journey">
+              Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus pellentesque elit.
+              Pellentesque eget ut imperdiet nulla penatibus. Nascetur viverra arcu sed amet cursus purus.
+            </p>
+          </div>
+          <button className="hero-button-journey">Open Account</button>
+    
+        </section>
+      </div>
+    </div>
+  );
+};
