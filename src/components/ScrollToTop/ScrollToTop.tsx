924a1822ea6dcd5c2daa87904248d0d227626f20
Ilya <ilyagliba02@gmail.com>
2025-06-26 21:28:30 +0300

    rename

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./ScrollToTop.css";
 import "aos/dist/aos.css";
 
@@ -20,7 +20,7 @@ export default function ScrollToTopButton() {
     if (!visible) return null;
 
     return (
-        <button className="scroll-to-top-btn" data-aos="zoom-in" onClick={handleClick} aria-label="Наверх">
+        <button className="scroll-to-top-btn" data-aos="zoom-in" onClick={handleClick} aria-label="Up">
             <svg id="Layer_1" className="scroll-ico" data-aos="fade-up" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115.4 122.88"><title>up-arrow</title><path d="M24.94,67.88A14.66,14.66,0,0,1,4.38,47L47.83,4.21a14.66,14.66,0,0,1,20.56,0L111,46.15A14.66,14.66,0,0,1,90.46,67.06l-18-17.69-.29,59.17c-.1,19.28-29.42,19-29.33-.25L43.14,50,24.94,67.88Z" /></svg>
         </button>
     );
